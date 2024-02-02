5faacc32106449b9945ffe9e9c183dc8541b20da
Mert Dalkılıç <116025152+MrTractorWheel@users.noreply.github.com>
2024-02-02 15:31:11 +0300

    Add files via upload

---
+++ Source/ToonTanks/Tower.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tower.h"
+#include "Tank.h"
+#include "Kismet/GameplayStatics.h"
+#include "TimerManager.h"
+
+void ATower::Tick(float DeltaTime){
+    Super::Tick(DeltaTime);
+    if(InRange()){
+        RotateTurret(Tank->GetActorLocation());
+    }
+}
+
+void ATower::HandleDestruction(){
+    Super::HandleDestruction();
+    Destroy();
+}
+
+void ATower::BeginPlay(){
+    Super::BeginPlay();
+    Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+    GetWorldTimerManager().SetTimer(FireRateHandle, this, &ATower::CheckFireCondition, FireRate, true);
+}
+
+void ATower::CheckFireCondition(){
+    if(InRange() && Tank && Tank->bAlive){
+        Fire();
+    }
+}
+
+bool ATower::InRange(){
+    if(Tank){
+        float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
+        if(Distance <= FireRange){
+            return true;
+        }
+    }
+    return false;
+}
